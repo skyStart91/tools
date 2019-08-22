dc91f395a695ee00648cce2eb1200c97019e8793
skyStart91 <35184691+skyStart91@users.noreply.github.com>
2019-08-22 16:52:39 +0800

    Update genrsa.sh

--- genrsa.sh
+++ genrsa.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 openssl version
 echo "server.key Start Creating..."
-read -p "Input Crt Length 1024 Or 2048:" LEN
+#20s没有输入就使用默认，默认是2048
+read -p "Input Crt Length 1024 Or 2048 (Default 2048):" -t 20 LEN
 openssl genrsa -out server.key $LEN
 echo "server.key Complete Created"
 echo "Request Crt Start Creating..."
@@ -9,7 +10,8 @@ subject="/C=CN/ST=AH/L=HF/O=YF/OU=LTECH"
 openssl req -new -key server.key -subj $subject -out server.csr
 echo "Request Completed"
 echo "Certificate Starting..."
-read -p "Please Input Effective Time(Unit By Day):" STIME
+#20s没有输入则使用默认，默认是空,如果此处使用默认则会报错
+read -p "Please Input Effective Time(Unit By Day):" -t 20 STIME
 openssl x509 -req -days $STIME -in server.csr -signkey server.key -out server.crt
 echo "Completed"
 echo "Change Authrization Starting..."
